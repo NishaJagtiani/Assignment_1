95f56063728a0c50241e42e332532e4e3bae6e6d
NishaJagtiani <nishajagtiani17@gmail.com>
2015-08-30 00:12:55 -0700

    more new features
    users online list working
    fixed formatting problems

--- Chat_Application/index.js
+++ Chat_Application/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
@@ -7,108 +8,141 @@ var fs = require('fs');
 var dbFile = './database.db';
 var chatID = 101;
 
+var usersonline = new Array();
+
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
+/* Directs to our Login page */
+app.get('/', function(req, res)
+{
   res.sendFile(__dirname + '/public/home.html');
 });
 
 
-fs.exists(dbFile, function(exists)
-	{
-		if(!exists)
-		{
-			console.log("yessss");
-			fs.open(dbFile, 'w', function(err, fd)
-			{
-				//if(err) throw err;
-				fs.close(fd);
-			});
+      /* Opens the existing DB if not existed creates a new one */
+      fs.exists(dbFile, function(exists)
+      	{
+      		if(!exists)
+      		{
+      			console.log("DB CONNECTED");
+      			fs.open(dbFile, 'w', function(err, fd)
+      			{
+      				//if(err) throw err;
+      				fs.close(fd);
+      			});
 
-			var db = new sqlite3.Database(dbFile);
-			db.run('CREATE TABLE `chatData` (' +
-    		'`PerId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' +
-    		'`Name` TEXT NOT NULL,' +
-    		'`Msg` TEXT,' +
-    		'`TDValue` DATETIME DEFAULT CURRENT_TIMESTAMP,' +
-    		'`ChatID` INTEGER NOT NULL)');
-    		db.close();
-		}
-	});
+      			var db = new sqlite3.Database(dbFile);
+      			db.run('CREATE TABLE `chatData` (' +
+          		'`PerId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' +
+          		'`Name` TEXT NOT NULL,' +
+          		'`Msg` TEXT,' +
+          		'`TDValue` DATETIME DEFAULT CURRENT_TIMESTAMP,' +
+          		'`ChatID` INTEGER NOT NULL)');
+          		db.close();
+      		}
+      });
 
-var db = new sqlite3.Database(dbFile);
 
-function getDateTime()
+
+    /* Time Date Display */
+    var db = new sqlite3.Database(dbFile);
+
+      function getDateTime()
+      {
+      var currentdate = new Date();
+
+       var datetime = currentdate.getFullYear() + "-"
+       + (currentdate.getMonth()+1) + "-"
+       + currentdate.getDate() + " "
+       + currentdate.getHours() + ":"  
+       + currentdate.getMinutes() + ":" 
+       + currentdate.getSeconds(); 
+
+      return datetime;
+      }
+
+
+
+
+      /* Inserts data into the table*/
+
+    function insertChat(username, msg)
+    {
+    	
+    	var statement = db.prepare('INSERT INTO `chatData` (`Name`, `Msg`, `TDValue`, `ChatID`) ' + 'VALUES (?, ?, ?, ?)');
+    	statement.run(username, msg , getDateTime(), chatID);
+    	statement.finalize();
+    }
+
+
+
+/* once the chat room connection establishes */
+
+io.on('connection', function(socket)
+  {
+
+        /* Displays the full chat mgs */ 
+        socket.on('chat message', function(msg)
+        {
+
+        	var datetime = getDateTime();
+        	//var fullmsg = socket.nickname + " : " + msg + " : " + datetime;
+        	var m = new Array();
+        	m.push(socket.nickname);
+            m.push(msg);
+            m.push(datetime);
+        	insertChat(socket.nickname, msg);
+          io.emit('chat message', m);
+        });
+
+
+
+
+
+        /* Entry of the new User in the chat room */ 
+         socket.on('joinchat', function(usrname)
+            {
+            	socket.nickname = usrname;
+            	console.log(usrname + ' has joined');
+            	usersonline.push(usrname);
+            	io.sockets.emit('onuserjoined' , usrname + ' has joined' );
+            	io.sockets.emit('updatelist' , usersonline);
+            	
+          	   var msgs = new Array();
+            	db.all("SELECT * FROM chatData", function(err, rows)
+            	{
+            		rows.forEach(function(row) 
+                {
+                		var m = new Array();
+                		m.push(row.Name);
+                		m.push(row.Msg);
+                		m.push(row.TDValue);
+            			//var m = row.Name + " : " + row.Msg + " @ " + row.TDValue;
+            			msgs.push(m);			
+            	  });
+
+            		socket.emit('chathistory', msgs);
+              });
+
+            });
+
+
+            /* users Exits */
+            socket.on('disconnect', function() 
+            {
+            	console.log(socket.nickname + ' has left');
+            	var indx = usersonline.indexOf(socket.nickname);
+            	usersonline.splice(indx, 1);
+            	io.sockets.emit('updatelist' , usersonline);
+            	io.sockets.emit('onuserleft' , socket.nickname + ' has left' );
+            });
+
+  });
+
+
+
+
+http.listen(3000, function()
 {
-var currentdate = new Date();
-
- var datetime = currentdate.getFullYear() + "-"
- + (currentdate.getMonth()+1) + "-"
- + currentdate.getDate() + " "
- + currentdate.getHours() + ":"  
- + currentdate.getMinutes() + ":" 
- + currentdate.getSeconds(); 
-
-/*
-var datetime = currentdate.getDate() + "-"
-                + (currentdate.getMonth()+1)  + "-" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-*/
-return datetime;
-}
-
-function insertChat(username, msg)
-{
-	// Insert some data using a statement:
-	var statement = db.prepare('INSERT INTO `chatData` (`Name`, `Msg`, `TDValue`, `ChatID`) ' + 'VALUES (?, ?, ?, ?)');
-	statement.run(username, msg , getDateTime(), chatID);
-	statement.finalize();
-}
-
-
-
-io.on('connection', function(socket){
-
-  socket.on('chat message', function(msg){
-
-  	var datetime = getDateTime();
-  	var fullmsg = socket.nickname + " : " + msg + " : " + datetime;
-  	insertChat(socket.nickname, msg);
-  	console.log(fullmsg);
-    io.emit('chat message', fullmsg);
-  });
-
-  socket.on('joinchat', function(usrname){
-
-  	socket.nickname = usrname;
-  	console.log(usrname + ' has joined');
-  	io.sockets.emit('onuserjoined' , usrname + ' has joined' );
-  	
-	var msgs = new Array();
-  	db.all("SELECT * FROM chatData", function(err, rows)
-  	{
-  		rows.forEach(function(row) {
-  			var m = row.Name + " : " + row.Msg + " @ " + row.TDValue;
-  			msgs.push(m);			
-  		});
-
-  		socket.emit('chathistory', msgs);
-  	});
-
-  	
-  	
-  });
-
-  socket.on('disconnect', function() {
-  	console.log(socket.nickname + ' has left');
-  	io.sockets.emit('onuserleft' , socket.nickname + ' has left' );
-  });
-
-});
-
-http.listen(3000, function(){
   console.log('listening on *:3000');
 });
